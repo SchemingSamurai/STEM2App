d908f8522ea9b23e187b8b0d0c4e243051532434
Rhuel67 <chrisrwalsh67@gmail.com>
2021-05-22 22:09:31 -0400

    Fully combined project sections

--- app/src/main/java/com/example/stem2app/MainActivity.java
+++ app/src/main/java/com/example/stem2app/MainActivity.java
@@ -20,10 +20,12 @@ import android.hardware.camera2.TotalCaptureResult;
 import android.hardware.camera2.params.StreamConfigurationMap;
 import android.media.Image;
 import android.media.ImageReader;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.Handler;
 import android.os.HandlerThread;
+import android.speech.tts.TextToSpeech;
 import android.util.Log;
 import android.util.Size;
 import android.util.SparseIntArray;
@@ -39,6 +41,7 @@ import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Locale;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
@@ -47,10 +50,16 @@ import android.view.Surface;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.chaquo.python.PyObject;
+import com.chaquo.python.Python;
+import com.chaquo.python.android.AndroidPlatform;
+
 public class MainActivity extends AppCompatActivity {
+    private TextToSpeech mTTS;
     private static final String TAG = "AndroidCameraApi";
-    private TextureView textureView;
     private static final SparseIntArray ORIENTATIONS = new SparseIntArray();
+    Python py;
+
     static {
         ORIENTATIONS.append(Surface.ROTATION_0, 90);
         ORIENTATIONS.append(Surface.ROTATION_90, 0);
@@ -59,69 +68,87 @@ public class MainActivity extends AppCompatActivity {
     }
 
     protected CameraDevice cameraDevice;
-    protected CameraCaptureSession cameraCaptureSessions;
-    protected CaptureRequest.Builder captureRequestBuilder;
     private Size imageDimension;
     private ImageReader imageReader;
-    private File file;
     private static final int REQUEST_CAMERA_PERMISSION = 200;
     private Handler mBackgroundHandler;
     private HandlerThread mBackgroundThread;
     private boolean running = false;
     private int photoCount;
+    ImageReader reader;
+
+    private CameraCaptureSession captureSession;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textureView = (TextureView) findViewById(R.id.texture);
-        assert textureView != null;
-        textureView.setSurfaceTextureListener(textureListener);
-        Button takePictureButton = (Button) findViewById(R.id.btn_takepicture);
-        assert takePictureButton != null;
+        if (! Python.isStarted()) {
+            Python.start(new AndroidPlatform(this));
+        }
+        py = Python.getInstance();
+
+
+        Button start = findViewById(R.id.beginBtn);
+        mTTS = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if (status == TextToSpeech.SUCCESS){
+                    int result = mTTS.setLanguage(Locale.ENGLISH);
+
+                    if(result == TextToSpeech.LANG_MISSING_DATA
+                            || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                        Log.e("Yiming", "Language not supported");
+                    }
+                    else{
+                        start.setEnabled(true);
+                    }
+                }
+                else{
+                    Log.e("Yiming", "Initialization failed");
+                }
+            }
+        });
+
+        reader = generateCaptureSession();
+        Log.d("test", (captureSession == null) +"");
+        Log.d("test", "builder = null?" + (reader == null));
 
         photoCount = 0;
         ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
         running = false;
 
-        TextView textView = this.findViewById(R.id.count);
-        textView.setText("" + photoCount);
-
         executorService.scheduleAtFixedRate(() -> {
-            if (running){
-                takePicture();
-                photoCount++;
-                textView.setText("" + photoCount);
+            if(captureSession == null || reader == null){
+                reader = generateCaptureSession();
+                Log.d("test", "captureSession = null? " + (captureSession == null));
+                Log.d("test", "builder = null? " + (reader == null));
             }
-        }, 0, 1, TimeUnit.SECONDS);
 
-        takePictureButton.setOnClickListener(new View.OnClickListener() {
+            if (running && captureSession != null) {
+                try {
+                    captureSession.capture(getBuilder(reader).build(), captureListener, mBackgroundHandler);
+                } catch (CameraAccessException e) {
+                    e.printStackTrace();
+                }
+                photoCount++;
+            }
+        }, 0, 2, TimeUnit.SECONDS);
+
+        start.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 running = !running;
+                if(running){
+                    start.setText(R.string.stopTxt);
+                } else {
+                    start.setText(R.string.beginTxt);
+                }
             }
         });
-    }
 
-    TextureView.SurfaceTextureListener textureListener = new TextureView.SurfaceTextureListener() {
-        @Override
-        public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
-            //open your camera here
-            openCamera();
-        }
-        @Override
-        public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height) {
-            // Transform you image captured size according to the surface width and height
-        }
-        @Override
-        public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
-            return false;
-        }
-        @Override
-        public void onSurfaceTextureUpdated(SurfaceTexture surface) {
-        }
-    };
+    }
 
     private final CameraDevice.StateCallback stateCallback = new CameraDevice.StateCallback() {
         @Override
@@ -131,10 +158,12 @@ public class MainActivity extends AppCompatActivity {
             cameraDevice = camera;
             //createCameraPreview();
         }
+
         @Override
         public void onDisconnected(CameraDevice camera) {
             cameraDevice.close();
         }
+
         @Override
         public void onError(CameraDevice camera, int error) {
             cameraDevice.close();
@@ -146,8 +175,6 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onCaptureCompleted(CameraCaptureSession session, CaptureRequest request, TotalCaptureResult result) {
             super.onCaptureCompleted(session, request, result);
-            Toast.makeText(MainActivity.this, "Saved:" + file, Toast.LENGTH_SHORT).show();
-            //createCameraPreview();
         }
     };
 
@@ -168,10 +195,10 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    protected void takePicture() {
-        if(null == cameraDevice) {
+    protected ImageReader generateCaptureSession() {
+        if (null == cameraDevice) {
             Log.e(TAG, "cameraDevice is null");
-            return;
+            return null;
         }
         CameraManager manager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
         try {
@@ -187,101 +214,72 @@ public class MainActivity extends AppCompatActivity {
                 height = jpegSizes[0].getHeight();
             }
             ImageReader reader = ImageReader.newInstance(width, height, ImageFormat.JPEG, 1);
-            List<Surface> outputSurfaces = new ArrayList<Surface>(2);
+            List<Surface> outputSurfaces = new ArrayList<Surface>(1);
             outputSurfaces.add(reader.getSurface());
-            outputSurfaces.add(new Surface(textureView.getSurfaceTexture()));
-            final CaptureRequest.Builder captureBuilder = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_STILL_CAPTURE);
-            captureBuilder.addTarget(reader.getSurface());
-            captureBuilder.set(CaptureRequest.CONTROL_MODE, CameraMetadata.CONTROL_MODE_AUTO);
-            // Orientation
-            int rotation = getWindowManager().getDefaultDisplay().getRotation();
-            captureBuilder.set(CaptureRequest.JPEG_ORIENTATION, ORIENTATIONS.get(rotation));
 
-            file = new File(Environment.getExternalStoragePublicDirectory((Environment.DIRECTORY_PICTURES))+
-                    "/pic" +
-                    System.currentTimeMillis() +
-                    ".jpg");
-            ImageReader.OnImageAvailableListener readerListener = new ImageReader.OnImageAvailableListener() {
-                @Override
-                public void onImageAvailable(ImageReader reader) {
-                    Image image = null;
-                    try {
-                        image = reader.acquireLatestImage();
-                        ByteBuffer buffer = image.getPlanes()[0].getBuffer();
-                        byte[] bytes = new byte[buffer.capacity()];
-                        buffer.get(bytes);
-                        save(bytes);
-                    } catch (FileNotFoundException e) {
-                        e.printStackTrace();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    } finally {
-                        if (image != null) {
-                            image.close();
-                        }
-                    }
-                }
-                private void save(byte[] bytes) throws IOException {
-                    /*
-                        THIS IS THE PLACE WHERE THE BYTE ARRAY IS COLLECTED
-                    */
-                    OutputStream output = null;
-                    try {
-                        output = new FileOutputStream(file);
-                        output.write(bytes);
-                    } finally {
-                        if (null != output) {
-                            output.close();
-                        }
-                    }
-                }
-            };
-            reader.setOnImageAvailableListener(readerListener, mBackgroundHandler);
-            cameraDevice.createCaptureSession(outputSurfaces, new CameraCaptureSession.StateCallback() {
+
+
+            final CameraCaptureSession.StateCallback callback = new CameraCaptureSession.StateCallback() {
                 @Override
                 public void onConfigured(CameraCaptureSession session) {
-                    try {
-                        session.capture(captureBuilder.build(), captureListener, mBackgroundHandler);
-                    } catch (CameraAccessException e) {
-                        e.printStackTrace();
-                    }
+                    captureSession = session;
+                    //session.setRepeatingRequest(captureBuilder.build(), captureListener, mBackgroundHandler);
                 }
+
                 @Override
                 public void onConfigureFailed(CameraCaptureSession session) {
                 }
-            }, mBackgroundHandler);
+            };
+
+            cameraDevice.createCaptureSession(outputSurfaces, callback, mBackgroundHandler);
+            return reader;
+
         } catch (CameraAccessException e) {
             e.printStackTrace();
+            return null;
         }
     }
 
-    protected void createCameraPreview() {
-        try {
-            SurfaceTexture texture = textureView.getSurfaceTexture();
-            assert texture != null;
-            texture.setDefaultBufferSize(imageDimension.getWidth(), imageDimension.getHeight());
-            Surface surface = new Surface(texture);
-            captureRequestBuilder = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
-            captureRequestBuilder.addTarget(surface);
-            cameraDevice.createCaptureSession(Arrays.asList(surface), new CameraCaptureSession.StateCallback(){
-                @Override
-                public void onConfigured(@NonNull CameraCaptureSession cameraCaptureSession) {
-                    //The camera is already closed
-                    if (null == cameraDevice) {
-                        return;
+    private CaptureRequest.Builder getBuilder(ImageReader reader) throws CameraAccessException {
+        final CaptureRequest.Builder captureBuilder = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_STILL_CAPTURE);
+        captureBuilder.addTarget(reader.getSurface());
+        captureBuilder.set(CaptureRequest.CONTROL_MODE, CameraMetadata.CONTROL_MODE_AUTO);
+        // Orientation
+        int rotation = getWindowManager().getDefaultDisplay().getRotation();
+        captureBuilder.set(CaptureRequest.JPEG_ORIENTATION, ORIENTATIONS.get(rotation));
+
+        ImageReader.OnImageAvailableListener readerListener = new ImageReader.OnImageAvailableListener() {
+            @Override
+            public void onImageAvailable(ImageReader reader) {
+                Image image = null;
+                try {
+                    image = reader.acquireLatestImage();
+                    ByteBuffer buffer = image.getPlanes()[0].getBuffer();
+                    byte[] bytes = new byte[buffer.capacity()];
+                    buffer.get(bytes);
+                    save(bytes);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    if (image != null) {
+                        image.close();
                     }
-                    // When the session is ready, we start displaying the preview.
-                    cameraCaptureSessions = cameraCaptureSession;
-                    updatePreview();
                 }
-                @Override
-                public void onConfigureFailed(@NonNull CameraCaptureSession cameraCaptureSession) {
-                    Toast.makeText(MainActivity.this, "Configuration change", Toast.LENGTH_SHORT).show();
-                }
-            }, null);
-        } catch (CameraAccessException e) {
-            e.printStackTrace();
-        }
+            }
+
+            private void save(byte[] bytes) throws IOException {
+
+                Log.d("test", "collecting results");
+                boolean surfaceChange = runAlgorithm(bytes);
+                Log.d("test",""+surfaceChange);
+                mTTS.speak(""+surfaceChange,TextToSpeech.QUEUE_FLUSH,null,null);
+            }
+        };
+        reader.setOnImageAvailableListener(readerListener, mBackgroundHandler);
+
+        return captureBuilder;
     }
 
     private void openCamera() {
@@ -303,18 +301,6 @@ public class MainActivity extends AppCompatActivity {
             e.printStackTrace();
         }
         Log.e(TAG, "openCamera X");
-    }
-
-    protected void updatePreview() {
-        if(null == cameraDevice) {
-            Log.e(TAG, "updatePreview error, return");
-        }
-        captureRequestBuilder.set(CaptureRequest.CONTROL_MODE, CameraMetadata.CONTROL_MODE_AUTO);
-        try {
-            cameraCaptureSessions.setRepeatingRequest(captureRequestBuilder.build(), null, mBackgroundHandler);
-        } catch (CameraAccessException e) {
-            e.printStackTrace();
-        }
     }
 
     private void closeCamera() {
@@ -344,11 +330,7 @@ public class MainActivity extends AppCompatActivity {
         super.onResume();
         Log.e(TAG, "onResume");
         startBackgroundThread();
-        if (textureView.isAvailable()) {
-            openCamera();
-        } else {
-            textureView.setSurfaceTextureListener(textureListener);
-        }
+        openCamera();
     }
 
     @Override
@@ -357,5 +339,15 @@ public class MainActivity extends AppCompatActivity {
         //closeCamera();
         stopBackgroundThread();
         super.onPause();
+    }
+
+    private boolean runAlgorithm(byte[] bytes) {
+        //create python object
+        PyObject pyObj = py.getModule("sample");
+
+        //call function, pass imageString as a parameter into the script
+        PyObject obj = pyObj.callAttr("isColorChange", bytes);
+
+        return obj.toBoolean();
     }
 }
---
+++ app/src/main/python/sample.py
@@ -0,0 +1,82 @@
+import os
+import io
+import PIL.Image as Image
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import json
+import math
+import cv2 as cv
+from matplotlib import pyplot as plt
+
+
+from array import array
+
+
+def areClose(color1, color2):
+  return sum(abs(color1 - color2)) < 50
+
+def areShades(color1, color2):
+  imax = (list(color1)+list(color2)).index(max(list(color1)+list(color2))) % 3
+  ratio = color1[imax] / color2[imax]
+  sum = 0
+  for i in range(3):
+    if i != imax:
+      sum += max([abs(color1[i] - color2[i] * ratio), abs(color1[i] / ratio - color2[i])])
+  return sum < 40
+
+
+def monotonicrow(i,j, m, n, avgimg):
+  if j <= 1 or j >= n-2:
+    return False
+  shades = True
+  for a in range(j-2,j+1):
+    for b in range(a+1,j+2):
+      if not areShades(avgimg[i][a], avgimg[i][b]):
+        shades = False
+  if not shades:
+    return True
+  for b in range(j-2, j+2):
+    for k in range(3):
+      if (avgimg[i][b+1][k] - avgimg[i][b][k]) * (avgimg[i][j+1][k] - avgimg[i][j][k]) < 0:
+        return False
+  return True
+
+def monotoniccol(i,j, m, n, avgimg):
+  if i <= 1 or i >= m-2:
+    return False
+  shades = True
+  for a in range(i-2,i+1):
+    for b in range(a+1,i+2):
+      if not areShades(avgimg[a][j], avgimg[b][j]):
+        shades = False
+  if not shades:
+    return True
+  for a in range(i-2,i+2):
+    for k in range(3):
+      if (avgimg[a+1][j][k] - avgimg[a][j][k]) * (avgimg[i+1][j][k] - avgimg[i][j][k]) < 0:
+        return False
+  return True
+
+def isColorChange(image):
+  m = 20
+  n = 15
+  ylow = 4
+  yhigh = 16
+  ydiff = yhigh-ylow
+  xlow = 3
+  xhigh = 12
+  xdiff = xhigh-xlow
+  img = np.asarray(Image.open(io.BytesIO(bytearray(image))))
+  avgimg = np.mean(np.mean(np.asarray([np.asarray([img[img.shape[0] // m * i: img.shape[0] // m * (i+1), img.shape[1] // n * j: img.shape[1] // n * (j+1)] for j in range(xlow, xhigh)]) for i in range(ylow, yhigh)]),axis = 2), axis = 2)
+  change = False
+  for i in range(yhigh-ylow-1):
+    for j in range(xhigh-xlow-1):
+      c1 = avgimg[i][j]
+      c2 = avgimg[i+1][j]
+      c3 = avgimg[i][j+1]
+      if not areClose(c1,c2) and (not areShades(c1,c2) or monotoniccol(i,j, ydiff, xdiff, avgimg)):
+        change = True
+      if not areClose(c1,c3) and (not areShades(c1,c3) or monotonicrow(i,j, ydiff, xdiff, avgimg)):
+        change = True
+  return change
